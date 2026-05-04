ee5e58909017001590d9d4681bfc414ae530e3a7
Abdielitis67 <meirdanz4@gmail.com>
2026-05-04 18:50:51 +0700

    buat akses

--- Staticfunction.cpp
+++ Staticfunction.cpp
@@ -4,3 +4,12 @@ using namespace std;
 
 class Mahasiswa { //membuat class Mahasiswa
 
+private:
+    static int nim;
+public:
+    int id;
+    string nama;
+
+    void setID();
+    void printAll();
+
